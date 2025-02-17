1bbacd1e01313404e55155495733522d1e281da9
wtchuigo <77848035+wtchuigo@users.noreply.github.com>
2025-02-17 17:05:28 +0100

    Update MemberMapper.java

--- src/main/java/com/wtchuigo/reunion/mapper/MemberMapper.java
+++ src/main/java/com/wtchuigo/reunion/mapper/MemberMapper.java
@@ -6,6 +6,7 @@ import com.wtchuigo.reunion.core.MemberDto;
 import com.wtchuigo.reunion.model.Member;
 
 @Mapper
+@SuppressWarnings("java:S106")
 public interface MemberMapper {
 	
 	MemberDto memberToMemberDto(Member member);
